18ef7144c562bcb9b5abc5bfdeeb2057ec3ba18d
Laycon Muriziq <layconmm@msn.com>
2022-03-22 21:26:03 -0500

    Update ordersController.js

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -23,18 +23,19 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  // update: function(req, res) {
-  //   return db.orders.create(req.body) 
-  //     .then(function(dbOrders) {
-  //       return db.user.findOneAndUpdate({ _id: req.params.id }, {orders: dbOrders._id}, {new:true}, req.body)
-  //   })
-  //     .then(dbOrders => res.json(dbOrders))
-  //     .catch(err => {
-  //       console.error('Error creating the orders!', err);
-  //       return res.status(422).json(err)
-  //     });
-  // },
- 
+  //****************I UNCOMMENT THIS BLOCK************************************************************************
+  update: function(req, res) {
+    return db.orders.create(req.body) 
+      .then(function(dbOrders) {
+        return db.user.findOneAndUpdate({ _id: req.params.id }, {orders: dbOrders._id}, {new:true}, req.body)
+    })
+      .then(dbOrders => res.json(dbOrders))
+      .catch(err => {
+        console.error('Error creating the orders!', err);
+        return res.status(422).json(err)
+      });
+  },
+ //****************************************************************************************************************
   
   remove: function(req, res) {
     db.orders
